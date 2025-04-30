1eef4eca0b5e1d9bc74697260c78659b0b236fac
IsaacMak <95964568+IsaacMak2009@users.noreply.github.com>
2025-04-30 14:00:25 +0800

    Update import_torch.py

--- import_torch.py
+++ import_torch.py
@@ -5,7 +5,6 @@ import pandas as pd
 import cv2
 from tqdm.notebook import tqdm
 import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 import matplotlib.image as mpimg
 %matplotlib inline
@@ -20,3 +19,8 @@ from torchvision import datasets, transforms, models
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import ToTensor
 from torchvision.utils import make_grid
+
+random.seed(42)
+np.random.seed(42)
+torch.manual_seed(42)
+torch.cuda.manual_seed_all(42)
